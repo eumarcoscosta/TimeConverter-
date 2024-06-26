c6f8ed62a1dafe6b3e1ceeb3d974eaadc7011cca
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-06-26 11:29:49 +0100

    Complete app

---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos, segundos, resto, valor;
+
+            Console.WriteLine("Insira o numero para conversão: ");
+
+            valor = int.Parse(Console.ReadLine());
+
+            horas = valor / 3600;
+            resto = valor % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
